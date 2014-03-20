940ab3afe19832baa18892b32f8b9c7ca2bc7b8e
Hugo Duncan <hugo@palletops.com>
2014-03-20 10:38:43 +0000

    "Updated version for next release cycle"

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-pallet-release "0.1.0"
+(defproject lein-pallet-release "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://github.com/palletops/lein-pallet-release"
   :license {:name "Eclipse Public License"
