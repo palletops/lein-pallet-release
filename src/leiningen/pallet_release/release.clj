a4d43ae71d3037656bd4999855d2193a260b6d04
Hugo Duncan <hugo@palletops.com>
2014-03-20 14:42:59 +0000

    Updated version for next release cycle

--- src/leiningen/pallet_release/release.clj
+++ src/leiningen/pallet_release/release.clj
@@ -20,7 +20,7 @@
   (git/add "ReleaseNotes.md"))
 
 (defn release-profiles [project]
-  {:dev {:plugins '[[lein-pallet-release "0.1.2"]]
+  {:dev {:plugins '[[lein-pallet-release "0.1.3-SNAPSHOT"]]
          :pallet-release (release-config project)}
    :no-checkouts {:checkout-deps-shares ^:replace []}
    :release {:set-version
