774ce8054cecc022aed6b600fd91301cbf67879e
Hugo Duncan <hugo@hugoduncan.org>
2014-07-13 10:23:32 -0400

    Updated project.clj, release notes and readme for 0.1.19

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-pallet-release "0.1.19-SNAPSHOT"
+(defproject lein-pallet-release "0.1.19"
   :description "A leiningen plugin for the PalletOps release workflow"
   :url "http://github.com/palletops/lein-pallet-release"
   :license {:name "Eclipse Public License"
